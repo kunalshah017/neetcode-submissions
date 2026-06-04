247078245fc6ca770c6004bed85d3612e5a15017
Kunal Shah <kunalmanishshah@gmail.com>
2026-06-05 00:56:24 +0530

    Add: is-anagram - submission-3

---
+++ Data Structures & Algorithms/is-anagram/submission-3.rs
@@ -0,0 +1,20 @@
+impl Solution {
+    pub fn is_anagram(s: String, t: String) -> bool {
+        if s.len() != t.len(){
+            return false
+        }
+
+        let mut s_vec: Vec<char> = s.chars().collect();
+        s_vec.sort_unstable();
+        
+        let mut t_vec: Vec<char> = t.chars().collect();
+        t_vec.sort_unstable();
+
+        if s_vec == t_vec {
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+}
