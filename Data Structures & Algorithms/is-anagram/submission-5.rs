32ed6797f2ff63ef1c377a0aace291932ade6e35
Kunal Shah <kunalmanishshah@gmail.com>
2026-06-05 00:58:11 +0530

    Add: is-anagram - submission-5

---
+++ Data Structures & Algorithms/is-anagram/submission-5.rs
@@ -0,0 +1,21 @@
+impl Solution {
+    pub fn is_anagram(s: String, t: String) -> bool {
+        if s.len() != t.len(){
+            return false
+        }
+
+        let mut s_vec: Vec<char> = s.chars().collect();
+        s_vec.sort();
+        
+        let mut t_vec: Vec<char> = t.chars().collect();
+        t_vec.sort();
+
+        for i in 0..s_vec.len(){
+            if s_vec[i] != t_vec[i]{
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
